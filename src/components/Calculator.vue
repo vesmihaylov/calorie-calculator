c3bff8badaf4d9a07d5a11b30bfc69a7fc7acdf2
Veselin Mihaylov <vesmihaylovdev@gmail.com>
2021-07-03 12:49:01 +0300

    feat(Calculator): add inspirational quotes

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,47 +1,52 @@
 <template>
   <div class="background">
     <div class="container">
-      <div class="d-flex justify-content-md-center align-items-center vh-100">
-          <div class="jumbotron bg-transparent">
-            <div class="form-group input-group-lg">
-              <label>Product calories <strong>(per 100g)</strong></label>
-              <input type="text" class="form-control" v-model="form.kcal" @keypress="isNumber($event)" id="kcal">
-              <br>
-              <label>Product carbohydrates <strong>(per 100g)</strong></label>
-              <input type="text" class="form-control" v-model="form.carbs" @keypress="isNumber($event)"
-                     id="carbs">
-              <br>
-              <label>Product fats <strong>(per 100g)</strong></label>
-              <input type="text" class="form-control" v-model="form.fats" @keypress="isNumber($event)" id="fats">
-              <br>
-              <label>Product protein <strong>(per 100g)</strong></label>
-              <input type="text" class="form-control" v-model="form.protein" @keypress="isNumber($event)"
-                     id="protein">
-              <br>
-              <label>How much grams did you eat?</label>
-              <input type="text" class="form-control" v-model="form.grams" @keypress="isNumber($event)"
-                     id="grams">
-            </div>
-            <hr>
-            <h3>Results</h3>
-            <table class="table bg-transparent table-borderless table-responsive">
-              <thead>
-              <tr>
-                <th scope="col">Total calories</th>
-                <th scope="col">Total carbohydrates</th>
-                <th scope="col">Total fats</th>
-                <th scope="col">Total protein</th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr>
-                <th>{{ (form.grams * form.kcal) / 100 }}</th>
-                <td>{{ (form.grams * form.carbs) / 100 }}</td>
-                <td>{{ (form.grams * form.fats) / 100 }}</td>
-                <td>{{ (form.grams * form.protein) / 100 }}</td>
-              </tr>
-              </tbody>
-            </table>
+      <blockquote class="blockquote text-center mt-5 mb-0">
+        <p class="mb-0">{{ quote.text }}</p>
+        <footer class="blockquote-footer">{{ quote.author }}</footer>
+        <hr>
+      </blockquote>
+      <div class="d-flex justify-content-md-center align-items-center">
+        <div class="jumbotron bg-transparent">
+          <div class="form-group input-group-lg">
+            <label>Product calories <strong>(per 100g)</strong></label>
+            <input type="text" class="form-control" v-model="form.kcal" @keypress="isNumber($event)" id="kcal">
+            <br>
+            <label>Product carbohydrates <strong>(per 100g)</strong></label>
+            <input type="text" class="form-control" v-model="form.carbs" @keypress="isNumber($event)"
+                   id="carbs">
+            <br>
+            <label>Product fats <strong>(per 100g)</strong></label>
+            <input type="text" class="form-control" v-model="form.fats" @keypress="isNumber($event)" id="fats">
+            <br>
+            <label>Product protein <strong>(per 100g)</strong></label>
+            <input type="text" class="form-control" v-model="form.protein" @keypress="isNumber($event)"
+                   id="protein">
+            <br>
+            <label>How much grams did you eat?</label>
+            <input type="text" class="form-control" v-model="form.grams" @keypress="isNumber($event)"
+                   id="grams">
+          </div>
+          <hr>
+          <h3>Results</h3>
+          <table class="table bg-transparent table-borderless table-responsive">
+            <thead>
+            <tr>
+              <th scope="col">Total calories</th>
+              <th scope="col">Total carbohydrates</th>
+              <th scope="col">Total fats</th>
+              <th scope="col">Total protein</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <th>{{ (form.grams * form.kcal) / 100 }}</th>
+              <td>{{ (form.grams * form.carbs) / 100 }}</td>
+              <td>{{ (form.grams * form.fats) / 100 }}</td>
+              <td>{{ (form.grams * form.protein) / 100 }}</td>
+            </tr>
+            </tbody>
+          </table>
 
         </div>
       </div>
@@ -59,9 +64,26 @@ export default {
         carbs: null,
         fats: null,
         protein: null,
-
+      },
+      quote: {
+        author: null,
+        text: null
       }
     }
+  },
+  created() {
+    var vm = this;
+    fetch("https://api.quotable.io/random?tags=success|faith|inspirational|wisdom|business|future")
+        .then(function (data) {
+          return data.json();
+        })
+        .then(function (data) {
+          vm.quote.text = data.content;
+          vm.quote.author = data.author;
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
   },
   methods: {
     isNumber: function (evt) {
